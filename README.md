189f62251cd31aa6883e20f41cf6f04b0c7c40b2
Pranay Kumar <pranay.rauthu@gmail.com>
2017-12-17 22:54:03 +0530

    added handler

--- README.md
+++ README.md
@@ -1,17 +1,24 @@
 # console.observe
 console utility function to observe changes in objects
 # How to use?
- 
+```javascript
     var obj = {};
-    console.observe("obj", this, {});
+    console.observe("obj", this, function(prop, value){
+        if(value){
+            console.log("obj modified");
+        }
+        else{
+            console.log("property accessed");
+        }
+    });
+```
 
 
 Now, try setting & getting values from <code>obj</code>
+```javascript
+    obj.name = "pranay";//obj modified
 
-    obj.name = "pranay";//setting name's value of obj to pranay
-
-
-    obj.name;//getting name from obj
-
+    obj.name;//property accessed
+```
 
 
--- console-observe.js
+++ console-observe.js
@@ -1,28 +1,41 @@
 // Object observer with ES-6 Proxy
-//accepts object name and context as parameter
-console.observe = function (objName, context, options) {
 
-    function logInfo(info) {
-        if (options.trace && console.trace) {
-            console.trace(info);
-        }
-        else {
-            console.log(info);
-        }
-    }
-    //handler for proxy
-    var handler = {
-        get: function (target, prop) {
-            logInfo(`getting ${prop} from ${objName}`);
-            //default behaviour
-            return Reflect.get(target, prop);
-        },
-        set: function (target, prop, value) {
-            logInfo(`setting ${prop}'s value of ${objName} to ${value}`);
-            //default behaviour
-            return Reflect.set(target, prop, value);
-        }
-    }
+(function (root, factory) {
+    
+    root.observe = factory();
 
-    context[objName] = new Proxy(context[objName], handler);
-}
+}(console, function () {
+    
+    /**
+     * 
+     * @param {String} objName - Name of the Object
+     * @param {Object} context - Observing context
+     * @param {Object} callback - Observing callback
+     */
+    function _observe(objName, context, callback) {
+        
+        //handler for proxy
+        var handler = {
+            get: function (target, prop) {
+                callback.call(context, prop)
+                //default behaviour
+                return Reflect.get(target, prop);
+            },
+            set: function (target, prop, value) {
+                callback(context, prop, value);
+                //default behaviour
+                return Reflect.set(target, prop, value);
+            }
+        }
+    
+        context[objName] = new Proxy(context[objName], handler);
+    };
+
+    return _observe;
+
+}));
+
+
+
+
+
